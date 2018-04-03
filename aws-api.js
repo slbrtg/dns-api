31485bb6dc213d6baec4cacf1e4c99cd2adc9ea7
slbrtg <slbrtg@gmail.com>
2018-04-02 17:02:42 -0700

    refactor aws api calls into functions, create call to create simple zone

--- aws-api.js
+++ aws-api.js
@@ -1,8 +1,30 @@
 const Route53 = require('aws-sdk/clients/route53');
-const constants = require('./aws-constants.js');
+const moment = require('moment');
 
 const route53 = new Route53({apiVersion: '2013-04-01'});
-let params = {};
-route53.getHostedZoneCount(params, (err, data) => {
-  err ? console.log(err, err.stack) : console.log(data);
-});
+
+
+function getHostedZoneCount() {
+  const params = {};
+  route53.getHostedZoneCount(params, (err, data) => {
+    err ? console.log(err, err.stack) : console.log(data);
+  });
+}
+
+function createSimpleHostedZone(zoneName) {
+    // Get UTC TIME FOR CALLER REFERENCE
+  const utcMoment = moment.utc();
+  const currentUtc = new Date(utcMoment.format()).toUTCString();
+  const params = {
+    Name: zoneName,
+    CallerReference: currentUtc
+  }
+
+  route53.createHostedZone(params, (err, data) => {
+    err ? console.log(err, err.stack) : console.log(data);
+  });
+}
+
+createSimpleHostedZone('example4.com')
+
+
